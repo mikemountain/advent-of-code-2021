91df46117e9a29cfc5b5af4b2645f092777c0323
Michael Milton <michaelrmilton5@gmail.com>
2021-12-17 19:24:56 -0500

    day 12 tidied up

--- 12Day/day_12.py
+++ 12Day/day_12.py
@@ -1,13 +1,11 @@
 from pprint import pprint
-# use a modified DAG?
-dag = {}
 
 def find_all_paths(graph, start, end, revisited = False, small_cave = '', path = []):
     path = path + [start]
     if start == end:
         return [path]
     paths = []
-    for node in graph[start]['caves']:
+    for node in graph[start]:
         if node.isupper():
             newpaths = find_all_paths(graph, node, end, revisited, small_cave, path)
             for newpath in newpaths:
@@ -28,18 +26,19 @@ def find_all_paths(graph, start, end, revisited = False, small_cave = '', path =
     return paths
 
 with open('input.txt') as f:
+    dag = {}
     lines = [line.strip() for line in f]
     for l in lines:
         cave_1, cave_2 = l.split('-')
         if cave_1 not in dag:
-            dag[cave_1] = {'caves': [cave_2]}
+            dag[cave_1] = [cave_2]
         else:
-            dag[cave_1]['caves'].append(cave_2)
+            dag[cave_1].append(cave_2)
 
         if cave_2 not in dag:
-            dag[cave_2] = {'caves': [cave_1]}
+            dag[cave_2] = [cave_1]
         else:
-            dag[cave_2]['caves'].append(cave_1)
+            dag[cave_2].append(cave_1)
     
     all_paths = find_all_paths(dag, 'start', 'end')
     print(len(all_paths))
